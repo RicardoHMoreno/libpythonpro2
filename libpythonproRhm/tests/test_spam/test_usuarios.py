6a79429f8ac5acb0156d22be41e6fc05223064b0
Ricardo <ricardo.moreno@electrolux.com>
2021-04-25 23:43:05 -0300

    Implementado @pytest.fixture para sessao e conexao

--- libpythonproRhm/tests/test_spam/test_usuarios.py
+++ libpythonproRhm/tests/test_spam/test_usuarios.py
@@ -1,25 +1,35 @@
+import pytest
+
 from libpythonproRhm.spam.db import Conexao
 from libpythonproRhm.spam.modelos import Usuario
 
 
-def test_savar_usuario():
-    conexao = Conexao()
-    sessao = conexao.gerar_sessao()
+@pytest.fixture
+def conexao():
+    #Setup
+    conexao_obj = Conexao()
+    yield conexao_obj
+    #Tear Down
+    conexao_obj.fechar()
+
+@pytest.fixture
+def sessao(conexao):
+    sessao_obj = conexao.gerar_sessao()
+    yield sessao_obj
+    sessao_obj.rollback()
+    sessao_obj.fechar()
+
+def test_savar_usuario(sessao):
     usuario = Usuario(nome='Renzo')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
-    sessao.rollback()
-    sessao.fechar()
-    conexao.fechar()
 
-
-def test_listar_usuario():
-    conexao = Conexao()
-    sessao = conexao.gerar_sessao()
+def test_listar_usuario(sessao):
     usuarios = [Usuario(nome='Renzo'), Usuario(nome='Luciano')]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
-    sessao.rollback()
-    sessao.fechar()
-    conexao.fechar()
+
+
+
+
