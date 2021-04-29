5a4724663388af1b31020fc1c0b92c6c7bfb229e
Ricardo <ricardo.moreno@electrolux.com>
2021-04-28 22:19:59 -0300

    Disponibilizando as fixture entre diferentes módudos com o uso do arquivo conftest.py

---
+++ libpythonproRhm/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -0,0 +1,11 @@
+from libpythonproRhm.spam.enviador_de_email import Enviador
+from libpythonproRhm.spam.main import EnviadorDeSpam
+
+
+def test_envio_de_spam(sessao):
+    enviador_de_spam= EnviadorDeSpam(sessao, Enviador())
+    enviador_de_spam.enviar_emails(
+        'renzo@python.pro.br',
+        'Curso Python Pro',
+        'Confira os módulos fantásticos'
+    )
--- libpythonproRhm/tests/test_spam/test_usuarios.py
+++ libpythonproRhm/tests/test_spam/test_usuarios.py
@@ -1,23 +1,5 @@
-import pytest
-
-from libpythonproRhm.spam.db import Conexao
 from libpythonproRhm.spam.modelos import Usuario
 
-
-@pytest.fixture
-def conexao():
-    #Setup
-    conexao_obj = Conexao()
-    yield conexao_obj
-    #Tear Down
-    conexao_obj.fechar()
-
-@pytest.fixture
-def sessao(conexao):
-    sessao_obj = conexao.gerar_sessao()
-    yield sessao_obj
-    sessao_obj.rollback()
-    sessao_obj.fechar()
 
 def test_savar_usuario(sessao):
     usuario = Usuario(nome='Renzo')
